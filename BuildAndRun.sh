85ea9287e1826d84f0bbeeb1b0c523b678e5591e
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2018-08-17 21:45:27 +0200

    Update BuildAndRun.sh

--- BuildAndRun.sh
+++ BuildAndRun.sh
@@ -1,2 +1,2 @@
 docker build --pull -t aspnetapp .
-docker run -e "AboutMessage=This is an About Message" --name aspnetcore_sample --rm -it -p 8000:80 aspnetapp
+docker run -e "AboutMessage=This is an About Message" --name aspnetcore_sample --rm -it -p 8000:80 aspnetapp 
